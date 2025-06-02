cff6fad563560e2e62f35d7b8aa3563e819835e3
tepukuliao <to.vegam@duocuc.cl>
2025-06-02 08:52:56 -0400

    ahora se guarda el fondo en el localstorage

---
+++ radioscl/coclea/static/emisoras/js/cambiar_fondo.js
@@ -0,0 +1,51 @@
+const fondos = [
+    {
+        url: "/static/img/fondo.gif",
+        tipo: "mosaico"
+    },
+    {
+        url: "/static/img/Fondo2.gif",
+        tipo: "mosaico"
+    },
+    {
+        url: "/static/img/black_hole.gif",
+        tipo: "completo"
+    }
+];
+
+let fondoActual = 0;
+
+function aplicarFondo(index) {
+    const fondo = fondos[index];
+    document.body.style.background = `url('${fondo.url}')`;
+
+    if (fondo.tipo === "mosaico") {
+        document.body.style.backgroundRepeat = "repeat";   
+        document.body.style.backgroundSize = "200px 200px";
+        document.body.style.backgroundColor = "#000"; 
+    }else{
+        document.body.style.backgroundRepeat = "no-repeat";
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundPosition = "center";
+        document.body.style.backgroundAttachment = "fixed";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    const boton = document.getElementById("cambiar-fondo");
+
+    const fondoGuardado = localStorage.getItem("fondoActual");
+    if (fondoGuardado !== null) {
+        fondoActual = parseInt(fondoGuardado);
+        aplicarFondo(fondoActual)
+    }
+
+    if(boton) {
+        boton.addEventListener("click", () => {
+            fondoActual = (fondoActual + 1) % fondos.length;
+            aplicarFondo(fondoActual);
+            
+            localStorage.setItem("fondoActual", fondoActual);
+        })
+    }
+})
